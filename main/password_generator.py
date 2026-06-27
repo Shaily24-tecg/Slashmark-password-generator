352e694e1bb82dff29dae21b1e490b461fb1a31d
Shaily <shaily15112005@gamil.com>
2026-06-27 10:33:47 +0530

    change mode append to write in the file opening mode to overwrite the file instead of appending to it. This ensures that each time new passwords are generated, the previous contents of the file are cleared and replaced with the new passwords.

--- main/password_generator.py
+++ main/password_generator.py
@@ -97,7 +97,11 @@ def main():
 
     print("\nGenerated Passwords:\n")
 
-    with open(file_name, "a") as file:
+    with open(file_name, "w") as file:
+        file.write("\n" + "=" * 40 + "\n")
+        file.write("NEW PASSWORDS\n")
+        file.write("=" * 40 + "\n")
+
         for i, pwd in enumerate(Password, start=1):
             strength = check_strength(pwd)
             print(f"Generated Password #{i}: {pwd} | Strength: {strength}")
